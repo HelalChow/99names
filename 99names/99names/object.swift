36a35ce5eb6a8a5530f14564ab595d7ceabacb57
hchowdhury3273 <hc2324@nyu.edu>
2020-04-16 23:23:25 -0400

    testing

--- 99names/99names/object.swift
+++ 99names/99names/object.swift
@@ -8,18 +8,29 @@
 
 import Foundation
 
-struct object{
+struct object: Codable{
     let name: String
     let transliteration: String
     let meaning: String
-    let status: String
     
     enum SerializationError:Error{
         case missing(String)
         case invalid(String, Any)
     }
-//
+
 //    init(json:[String:Any]) throws {
-//        
+//        guard let name = json["name"] as? String else {throw SerializationError.missing("name is missing")}
+//        guard let transliteration = json["transliteration"] as? String else {throw SerializationError.missing("name is missing")}
+//        guard let meaning = json["meaning"] as? String else {throw SerializationError.missing("name is missing")}
+//        self.name = name
+//        self.transliteration = transliteration
+//        self.meaning = meaning
+//    }
+//
+//    static let basePath = "http://api.aladhan.com/asmaAlHusna/"
+//
+//    static func getNames(completion: @escaping ([object])) -> ()) {
+//        let update = basePath + "1"
+//        guard let url = URL()
 //    }
 }
