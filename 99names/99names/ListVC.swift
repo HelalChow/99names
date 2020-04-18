c67ff6cd30a50667ca49ffb3c3628ded3d7415f7
hchowdhury3273 <hc2324@nyu.edu>
2020-04-17 23:40:01 -0400

    still not working

--- 99names/99names/ListVC.swift
+++ 99names/99names/ListVC.swift
@@ -11,7 +11,7 @@ import UIKit
 class ListVC: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
-    var array = [object]()
+    var array = [first]()
     
     let names = [
         "Ar-Rahman",
@@ -25,20 +25,15 @@ class ListVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        fillArray {
-            print(self.array.count)
-            self.tableView.reloadData()
+        fillArray{
+            print(self.array)
         }
-        print(self.array.count)
-//        object.getNames() {(results:[object]) in
-//            self.array.append(results[0])
-//            print("\(results[0])")
-//        }
-        tableView.reloadData()
+
+        print(array)
         
+        print(array.count)
         tableView.delegate = self
         tableView.dataSource = self
-        
 
     }
    
@@ -48,29 +43,19 @@ class ListVC: UIViewController {
         let url = basePath + "1"
         let request = URLRequest(url: URL(string: url)!)
 
-
         URLSession.shared.dataTask(with: request) { (data, response, err) in
-//                    var entry:[object] = []
+
             guard let data = data else {return}
             do {
-                // This line would allow to replace everything else below. And the init
-                // let name = try JSONDecoder().decode(object.self, from: data)
-
-                guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] else {return}
-                guard let item = json["data"] as? [[String:Any]] else {print("item not found"); return}
-
-//                self.name = try object(json: item[0])
-//                self.array.append(self.name)
-//                print(self.name)
-                self.array.append(try object(json: item[0]))
+                self.array = [try JSONDecoder().decode(first.self, from: data)]
                 DispatchQueue.main.async {
                     completed()
                 }
 
-
             } catch let jsonErr {
                 print("Error Serializing json", jsonErr)
             }
+
 
         }.resume()
     }
@@ -86,7 +71,7 @@ extension ListVC: UITableViewDelegate{
 
 extension ListVC: UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return array.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
--- 99names/99names/object.swift
+++ 99names/99names/object.swift
@@ -9,50 +9,29 @@
 import Foundation
 
 
-struct object{
+struct first:Decodable{
+    let data: [second]
+}
+
+struct second:Decodable {
     let name: String
     let transliteration: String
-    let meaning: String
+    let en: third
     
     enum SerializationError:Error{
         case missing(String)
         case invalid(String, Any)
     }
 
-    init(json:[String:Any]) throws {
-        name = json["name"] as? String ?? ""
-        transliteration = json["transliteration"] as? String ?? ""
-        guard let en = json["en"] as? [String:Any] else {throw SerializationError.missing("en is missing")}
-        meaning = en["meaning"] as? String ?? ""
-    }
-
-//    static let basePath = "http://api.aladhan.com/asmaAlHusna/"
-//
-//    static func getNames(completion: @escaping ([object]) -> ()) {
-//        let url = basePath + "1"
-//        let request = URLRequest(url: URL(string: url)!)
-//
-//
-//        URLSession.shared.dataTask(with: request) { (data, response, err) in
-//            var entry:[object] = []
-//            guard let data = data else {return}
-//            do {
-//                // This line would allow to replace everything else below. And the init
-////                let name = try JSONDecoder().decode(object.self, from: data)
-//
-//                guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] else {return}
-//                guard let item = json["data"] as? [[String:Any]] else {print("item not found"); return}
-//
-//                let name = try object(json: item[0])
-//                entry.append(name)
-//
-//
-//
-//            } catch let jsonErr {
-//                print("Error Serializing json", jsonErr)
-//            }
-//            completion(entry)
-//
-//        }.resume()
+//    init(json:[String:Any]) throws {
+//        name = json["name"] as? String ?? ""
+//        transliteration = json["transliteration"] as? String ?? ""
+//        guard let en = json["en"] as? [String:Any] else {throw SerializationError.missing("en is missing")}
+////        meaning = en["meaning"] as? String ?? ""
 //    }
+
+}
+
+struct third:Decodable {
+    let meaning:String
 }
