66b259f2bc5f4f4042490f555c7cbeb62bdfb52b
hchowdhury3273 <hc2324@nyu.edu>
2020-04-20 18:26:18 -0400

    carousel

--- 99names/99names/ViewControllers/StudyVC.swift
+++ 99names/99names/ViewControllers/StudyVC.swift
@@ -9,20 +9,78 @@
 import UIKit
 
 class StudyVC: UIViewController {
+//    let data =[]
     
-    @IBOutlet weak var collectionView: UICollectionView!
+    fileprivate let collectionView: UICollectionView = {
+       let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.register(CustomCell.self, forCellWithReuseIdentifier: "collection")
+        return cv
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        collectionView.register(UINib.init(nibName: "studyCell", bundle: nil), forCellWithReuseIdentifier: "studyCell")
-//        let flowLayout = UP
-        
-        
-       
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .clear
+        collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 300).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10).isActive = true
+//        collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -40).isActive = true
+
+        collectionView.heightAnchor.constraint(equalTo: collectionView.widthAnchor, multiplier: 1).isActive = true
+
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    
     }
     
-   
-   
+}
+extension StudyVC: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width/1.5, height: collectionView.frame.width/1.5)
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collection", for: indexPath) as! CustomCell
+        cell.backgroundColor = .clear
+        return cell
+    }
 }
 
+class CustomCell: UICollectionViewCell{
+    
+    var data: first? {
+        didSet{
+            guard let data = data else {return}
+//            bg.image = data.data[0].transliteration
+        }
+    }
+    
+    fileprivate let bg: UIImageView = {
+        let iv = UIImageView()
+        iv.image = #imageLiteral(resourceName: "sky")
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.layer.cornerRadius = 40
+        return iv
+    }()
+    
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        contentView.addSubview(bg)
+        bg.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        bg.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        bg.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+        bg.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
