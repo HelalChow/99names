12531c8208f9aa21a49d61395267e61ff598f588
hchowdhury3273 <hc2324@nyu.edu>
2020-04-23 19:18:23 -0400

    fatal error

--- 99names/99names/ViewControllers/CarouselVC.swift
+++ 99names/99names/ViewControllers/CarouselVC.swift
@@ -10,18 +10,22 @@ import UIKit
 
 class CarouselVC: UIViewController {
 
-    @IBOutlet weak var collectionView: UICollectionView!
+
+    @IBOutlet weak var collectView: UICollectionView!
     
     let dataArray = ["AAA", "BBB", "CCC", "DDD", "EEE", "FFF", "GGG"]
+    
+    var estimateWidth = 160.0
+    let cellMarginSize = 16.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
-//        self.collectionView.delegate = self
-        self.collectionView.dataSource = self
-        
-        self.collectionView.register(UINib(nibName: "itemCell", bundle: nil), forCellWithReuseIdentifier: "itelCell")
+//        self.collectView.delegate = self
+        self.collectView.dataSource = self
+//
+        self.collectView.register(UINib(nibName: "itemCell", bundle: nil), forCellWithReuseIdentifier: "itelCell")
     }
     
 
@@ -30,12 +34,26 @@ extension CarouselVC: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return self.dataArray.count
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "itemCell", for: indexPath) as! itemCell
-        cell.setData(text: self.dataArray[indexPath.row])
+        cell.setData(text: self.dataArray[0])
         return cell
     }
-    
-    
+}
+
+extension CarouselVC: UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = self.calculateWith()
+        return CGSize(width: width, height: width)
+    }
+    func calculateWith() -> CGFloat {
+        let estimatedWidth = CGFloat(estimateWidth)
+        let cellCount = floor(CGFloat(self.view.frame.size.width / estimatedWidth))
+
+        let margin = CGFloat(cellMarginSize * 2)
+        let width = (self.view.frame.size.width - CGFloat(cellMarginSize) * (cellCount - 1) - margin) / cellCount
+
+        return width
+    }
 }
